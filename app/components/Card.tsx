13bc371d39d135a63e3b65e1d825cc3a21f1050d
brunoLloret <caballoret@gmail.com>
2024-06-03 18:32:19 -0400

    first draft architecture of my personal wepage

---
+++ app/components/Card.tsx
@@ -0,0 +1,7 @@
+const Card = ({title}: {title: string}) => {
+    return <div className='text-xl font-bold'>
+            <h1>{title}</h1>
+        </div>
+}
+
+export default Card
--- app/routes/EqualZero.tsx
+++ app/routes/EqualZero.tsx
@@ -2,9 +2,15 @@ export default function EqualZero () {
     return (
         <>
         <div>
-            <h2>
-                =0 is an interactive fictional experience. Here, you follow a shapeshifter as it enters into a valley under military occupation. ='s journey will transform the valley's dynamics forever.
+            <h2
+            className= 'text-xl font-semibold padding: 20px'
+            >
+                =0 is an interactive fictional experience.
             </h2>
+            <p>
+            Here, you follow a shapeshifter as it enters into a valley under military occupation. ='s journey will transform the valley's dynamics forever.
+            </p>
+            <a href=""> Check it out</a>
 
         </div>
         </>
